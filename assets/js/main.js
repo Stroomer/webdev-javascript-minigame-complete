7229c16df049910ce9ca2a7e4049a5fc589abb39
Stroomer <lstroomer@glu.nl>
2022-05-19 09:45:57 +0200

    afbeeldingen inladen met JS

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+let canvas = null;
+let ctx    = null;
+
+let lada1  = null;
+let lada2  = null;
+let lada3  = null; 
+
+window.onload = async function() {
+	canvas = document.getElementById('game');
+	ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+
+	// METHODE 1: Inladen van een plaatje (onload)
+	// --------------------------------------
+
+	
+
+	// --------------------------------------
+
+
+	// METHODE 2: Inladen van een plaatje (XMLHttpRequest)
+	// --------------------------------------
+
+	
+
+	// --------------------------------------
+
+
+	// METHODE 3: Inladen van een plaatje (Promise)
+	// --------------------------------------
+	
+	
+
+	// --------------------------------------	  
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+stap_03_loading_images: 
+Afbeeldingen inladen en laten zien.
+-----------------------------------------------------------------------------------
+In deze stap ga je leren hoe je afbeeldingen kan inladen.
+
+- Je leert hoe je op drie verschillende manieren een afbeelding kan inladen.
+- Je leert hoe je een afbeelding kan tekenen op de <canvas></canvas>.
+- Je leert hoe je paden van assets kan opgeven en kan navigeren door de mapjes met / en ../
+- Je leert hoe je een afbeelding kunt opblazen/vergroten met behulp van drawImage()
+- Je leert hoe je een geblurde pixel-afbeelding netjes en scherp op de canvas getekend kan krijgen
